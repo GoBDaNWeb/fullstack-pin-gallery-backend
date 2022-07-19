adeb59739bfbebfd68beafc1681294b5be840ddc
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2022-07-20 02:48:26 +0300

    fix db connect

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ import commentsRouter from './routers/commentsRouter.js'
 
 const app = express()
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+ });
+
 app.use('/uploads', express.static('uploads'))
 app.use(express.json())
 app.use(cors())
